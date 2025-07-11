bce2e21c0632e16419c895c58f0de160bcf8279f
HatakeyamaNaoki <hatake.hatake.hatake7@outlook.jp>
2025-07-11 17:50:56 +0900

    Googleドライブ変更

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 SERVICE_ACCOUNT_FILE = '/etc/secrets/credentials.json'
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 CSV_FORMAT_PATH = '集計フォーマット.csv'
-SHARED_DRIVE_ID = "15tyS6xLu203jttUZlxllyuhQbtKHrXjN"
+SHARED_DRIVE_ID = "0AGgACoeUF81eUk9PVA"
